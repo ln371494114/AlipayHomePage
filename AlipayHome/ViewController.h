d2e372d80802b412905f3d1e841343b3b2380884
李楠 <ln371494114@163.com>
2018-07-20 10:29:58 +0800

    Initial Commit

---
+++ AlipayHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlipayHome
+//
+//  Created by 李楠 on 2018/7/20.
+//  Copyright © 2018年 李楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlipayHome/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AlipayHome
+//
+//  Created by 李楠 on 2018/7/20.
+//  Copyright © 2018年 李楠. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
